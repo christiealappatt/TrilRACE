20cdd35170997078a2acc1668cb6d8ca31dee233
Jonas Thies <16190001+jthies@users.noreply.github.com>
2022-12-09 18:55:26 +0100

    TrilinosRACE: need some source file to get the package to build

---
+++ packages/TrilinosRACE/src/dummy.cpp
@@ -0,0 +1,21 @@
+#include "RACE_CRS_raw.hpp"
+#include "RACE_frontend.hpp"
+#include "RACE_GmresPolyPreconKernel.hpp"
+#include "RACE_GmresSstepKernel.hpp"
+#include "RACE_kernels.hpp"
+#include "RACE_MGSmootherKernel.hpp"
+#include "RACE_packtype.hpp"
+#include "RACE_Precon.hpp"
+#include "RACE_pre_process.hpp"
+#include "RACE_SpMV.hpp"
+
+#include <iostream>
+
+namespace {
+
+void dummy()
+{
+  std::cout << "Hello World!" << std::endl;
+}
+
+}
